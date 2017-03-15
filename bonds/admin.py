7f34d94d43ba61fed71390bc1ed7610a1049aa48
Varun Mathur <vkmathur97@yahoo.co.uk>
2017-03-15 17:10:09 +0000

    update admin to include all models

--- bonds/admin.py
+++ bonds/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from .models import Syndicate
-from .models import PremiumBond
-from .models import UserProfile
+from .models import *
+
 
 # Register your models here.
 admin.site.register(Syndicate)
 admin.site.register(PremiumBond)
 admin.site.register(UserProfile)
+admin.site.register(Account)
+admin.site.register(ProductInfo)
+admin.site.register(ChatMessage)
+admin.site.register(Transaction)
