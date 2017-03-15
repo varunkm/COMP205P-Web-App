be944c466dcb7189daba83a5c28f140f3cbf92aa
Varun Mathur <vkmathur97@yahoo.co.uk>
2017-03-15 16:10:26 +0000

    static files

--- syndicate/settings.py
+++ syndicate/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT ='static'
+STATIC_ROOT ='staticfiles'
 STATIC_URL  ='/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
