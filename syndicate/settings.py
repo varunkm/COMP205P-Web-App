b526edbc3a4c4dd5ace0c879c2cc02dc71017c9d
Varun Mathur <vkmathur97@yahoo.co.uk>
2017-03-15 16:37:54 +0000

    idk what im doing

--- syndicate/settings.py
+++ syndicate/settings.py
@@ -14,8 +14,6 @@ import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT =os.path.join(BASE_DIR,'syndicate/static')
-STATIC_URL  ='/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -122,3 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT =os.path.join(BASE_DIR,'syndicate/static')
+STATIC_URL  ='/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
